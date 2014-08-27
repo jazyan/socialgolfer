87922bc4a2fb54a58093f4f5152a3429686d2e86
Jasmine Yan <jasmineyan@college.harvard.edu>
2014-08-27 15:11:32 -0400

    IT WORKS! prime powers that it

--- affine.py
+++ affine.py
@@ -3,12 +3,11 @@ import random
 
 order = 5
 def field_prime (order):
-    scalar = [i for i in range(order) if i != 0]
     c_scalar = [i for i in range(order)]
     points = [(i, j) for i in range(order) for j in range(order)]
     equ_class = [(1, b) for b in range(order)] + [(0, 1)]
     lines = {(a, b, c):[] for (a, b) in equ_class for c in c_scalar}
-    nums = {(a, b):i for (i, (a, b)) in enumerate(points)}
+    nums = {pt:i for (i, pt) in enumerate(points)}
     for (x, y) in points:
         for (a, b, c) in lines:
             if (a*x + b*y + c)%order == 0:
@@ -17,6 +16,8 @@ def field_prime (order):
         print "-----"
         for c in c_scalar:
             print (a, b, c), lines[(a, b, c)]
+
+print field_prime(5)
 
 def rem_lead_0 (li):
     to_str = ''.join(map(str, li)).rstrip("0")
@@ -65,35 +66,26 @@ def gen_irreducible (deg, p):
         divisors += div
     divisors = [d for d in divisors if sum(d) > (p-1)]
     divisors.append((0, 1))
-    print "DIVISORS", divisors
+    #print "DIVISORS", divisors
     score = 0
     while score != len(divisors):
         score = 0
         poly = rand_deg_poly (deg, p)
-        print "POLY", poly
+        #print "POLY", poly
         for d in divisors:
-            print d
+            #print d
             if polydiv(poly, d, p) != None:
-                print "FAILED", d, polydiv(poly, d, p)
+                #print "FAILED", d, polydiv(poly, d, p)
                 break;
             else:
                 score += 1
     return poly
 
-irred_poly = gen_irreducible (3, 2)
-print "poly", irred_poly
-
 def fieldelts (p, n):
     acc = [i for i in range(p)]*n
-    print "ACC", acc
     ans = list(set(itertools.combinations(acc, n)))
     assert len(ans) == pow(p, n)
     return ans
-
-elts = fieldelts(2, 3)
-print "elts", elts
-mapped = {i:elts[i] for i in range(len(elts))}
-print mapped
 
 def polymult (p1, p2, p):
     ans = [0]*(len(p1)+len(p2)-1)
@@ -103,40 +95,70 @@ def polymult (p1, p2, p):
     ans = rem_lead_0(ans)
     return ans
 
-print polymult([1, 1, 0, 1], [1, 1, 0], 2)
-
 # if len(ans) >= len(irr)
 def polymod (poly, irr, p):
     deg = len(poly) - len(irr)
     while deg >= 0:
-        print "POLY", poly
         x_deg = [0 for i in range(deg + 1)]
         for i in range(p):
             if (irr[-1]*i)%p == poly[-1]:
                 coeff = i
         x_deg[-1] = coeff
         mod = polymult(x_deg, irr, p)
-        print "MOD", mod
         poly = [(poly[i] - mod[i])%p for i in range(len(poly))]
         poly = rem_lead_0(poly)
         deg = len(poly) - len(irr)
     return poly
-
-print polymod([1, 1, 0, 1, 1], [1, 1, 0, 1], 2)
+#print polymod([1, 1, 0, 1, 1], [1, 1, 0, 1], 2)
 
 def polymultmod (p1, p2, irr, p):
     poly = polymult(p1, p2, p)
     poly = polymod(poly, irr, p)
+    if poly == []:
+        poly = [0]
     return poly
 
-def multtable (irr, elts):
+def multtable (irr, elts, p):
     L = len(elts)
     table = [[0 for i in range(L)] for j in range(L)]
     for i in range(L):
-        for j in range(L):
-            pass
-    print table
+        for j in range(i, L):
+            table[i][j] = polymultmod(elts[i], elts[j], irr, p)
+            table[j][i] = table[i][j]
+    return table
 
-    print table[0][0]
+def field_prime_power (p, n):
+    irred_poly = gen_irreducible (n, p)
+    elts = fieldelts(p, n)
+    mapped = {i:elts[i] for i in range(len(elts))}
+    print mapped
+    table = multtable(irred_poly, mapped, p)
 
-#print multtable(irred_poly, mapped)
+    nums = {v:k for k, v in mapped.items()}
+    print "NUMS", nums, type((0, 1))
+    iden_0 = tuple([0 for i in range(n)])
+    print iden_0
+    iden_1 = tuple([1] + [0 for i in range(n-1)])
+
+    points = [(i, j) for i in elts for j in elts]
+    enum = {pt:i for (i, pt) in enumerate(points)}
+    equ_class = [(iden_1, e) for e in elts] + [(iden_0, iden_1)]
+    print equ_class
+    lines = {(a, b, c):[] for (a, b) in equ_class for c in elts}
+    print lines
+    for (x, y) in points:
+        for (a, b, c) in lines:
+            L = len(c)
+            ax = polymultmod(a, x, irred_poly, p)
+            ax += [0 for i in range(L-len(ax))]
+            by = polymultmod(b, y, irred_poly, p)
+            by += [0 for i in range(L-len(by))]
+            ans = [(ax[i] + by[i] + c[i])%p for i in range(L)]
+            if tuple(ans) == iden_0:
+                lines[(a, b, c)].append(enum[(x, y)])
+    for (a, b) in equ_class:
+        print "-----"
+        for c in elts:
+            print (a, b, c), lines[(a, b, c)]
+
+field_prime_power (2, 3)
